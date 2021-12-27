3da61b8731cb6aa2c95a18f2c59fae56364a3894
Shrapan <samsureshkelkar@gmail.com>
2021-12-27 17:12:53 +0530

    implemented getCategory() to get extension

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let fs=require("fs");
 let path=require("path");
 let inputArr =process.argv.slice(2);
+let types={
+    photo:["jpg","jpeg"],
+    media:["mp4","mkv","3gp"],
+    archives:["zip","7z","rar","tar","gz","ar","iso","xz"],
+    documents:["docx","doc","pdf","xlsx","xls","odt","ods","odp","odg","odf","txt","ps","tex","json"],
+    app:["exe","dmg","pkg","deb","js","msi"]
+}
 let command=inputArr[0];
 switch(command)
 {
@@ -52,18 +59,35 @@ function organiseHelper(src,dest)
 let childName=fs.readdirSync(src);
 // console.log(childName);
 for(let i=0;i<childName.length;i++)
-{
+    {
     let childAdd=path.join(src,childName[i]);
     let isFile=fs.lstatSync(childAdd).isFile();
-    if(isFile){
-console.log(childName[i]);
-    } 
+    if(isFile)
+            {
+              // console.log(childName[i]);
+            //   console.log(path.extname(childName[i]));
+                let category=getCategory(childName[i]);
+                console.log(childName[i],"i belong to --->",category)
+            } 
+    }
+
 }
+function getCategory(name)
 {
-
+    let ext=path.extname(name);
+    ext=ext.slice(1);
+    for(let type in types)
+    {
+        let cTypeArray=types[type];
+        for(let i=0;i<cTypeArray.length;i++)
+        {
+            if(ext==cTypeArray[i]){
+                return type;
+            }
+        }
+    }
+    return "other";
 }
-}
-
 
 
 //help implemented
